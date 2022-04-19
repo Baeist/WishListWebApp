0e9d850d4b464ef85742141ed1e6be8234731579
Baeist <lars18n2@stud.kea.dk>
2022-04-19 12:38:12 +0200

    Initial

---
+++ src/main/java/com/example/wishlistwebapplication/WishListWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishlistwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishListWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wishlistwebapplication/WishListWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wishlistwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

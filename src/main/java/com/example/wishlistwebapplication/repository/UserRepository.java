cc5088e1a2742e67fad3e4190d885361744389d7
RedCupCrowe <valdemarkjohansen@gmail.com>
2022-04-20 21:45:38 +0200

    Oprettelse af database connections, metode til at oprette nye wishes, model classes til både user og wish.

--- src/main/java/com/example/wishlistwebapplication/repository/UserRepository.java
+++ src/main/java/com/example/wishlistwebapplication/repository/UserRepository.java
@@ -1,8 +1,58 @@
 package com.example.wishlistwebapplication.repository;
 
+import com.example.wishlistwebapplication.model.User;
 import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class UserRepository {
+
+  private String DBURL = "jdbc:mysql://localhost:3306/wishlist";
+  private String user = "root";
+  private String passWord = "2702";
+
+  private Connection connection;
+
+  public UserRepository() {
+    setConnection();
+  }
+
+  public void setConnection() {
+    try {
+      connection = DriverManager.getConnection(DBURL, user, passWord);
+
+    } catch (Exception e) {
+      System.out.println("Did not connect to database" + e);
+    }
+
+  }
+  public ArrayList<User> getAllUsers() {
+    ArrayList<User> userList = new ArrayList<>();
+
+    try {
+
+      Statement statement = connection.createStatement();
+      final String SQL_QUERY = "SELECT * FROM users";
+      ResultSet rs = statement.executeQuery(SQL_QUERY);
+
+
+      while (rs.next()) {
+        int userID = rs.getInt(1);
+        String username = rs.getString(2);
+        userList.add(new User(userID,username));
+
+      }
+
+      statement.close();      // god afslutning, har ingen reel betydning her
+    } catch (
+        SQLException e) {
+      System.out.println("Could not create connection");
+      e.printStackTrace();
+    }
+    return userList;
+  }
 
 }

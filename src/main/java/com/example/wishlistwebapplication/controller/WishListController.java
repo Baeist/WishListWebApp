bbaea6281a3bcd40a4a909afd34e7887efe385c9
tobiasras <tobiasras.job@gmail.com>
2022-04-21 20:36:39 +0200

    updated wishlist model, and wishlist controller: can show all wishlist cards

--- src/main/java/com/example/wishlistwebapplication/controller/WishListController.java
+++ src/main/java/com/example/wishlistwebapplication/controller/WishListController.java
@@ -1,19 +1,72 @@
 package com.example.wishlistwebapplication.controller;
 
+import com.example.wishlistwebapplication.model.WishList;
+import com.example.wishlistwebapplication.repository.UserRepository;
+import com.example.wishlistwebapplication.service.UserService;
 import com.example.wishlistwebapplication.service.WishListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
 
 @Controller
 public class WishListController {
-  WishListService wishListService;
 
-  public WishListController(WishListService wishListService) {
-    this.wishListService = wishListService;
-  }
 
-  @GetMapping("/wish_form")
-  public String wish_form() {
-    return "wish_form";
-  }
+    @Autowired
+    private UserService userService;
+    WishListService wishListService;
+
+    public WishListController(WishListService wishListService) {
+        this.wishListService = wishListService;
+    }
+
+    @GetMapping("/wish_form")
+    public String wish_form() {
+        return "wish_form";
+    }
+
+
+    //@GetMapping("/wishlist/user/{userID}")
+    @GetMapping("/wishlist/user/{userID}")
+    public String showAllWishlist(@PathVariable("userID") int userID, Model model) {
+        //User user = userService.getUserFromID(userID)
+        // Arraylist<Wishlist> wishlist = wishListService.getWishlist(userID);
+        model.addAttribute("username", "Tobias");
+        model.addAttribute("wishList", testWish());
+        return "userWishlist";
+    }
+
+    // todo Skal slette bruges kun til test
+    public ArrayList<WishList> testWish() {
+        ArrayList<WishList> list = new ArrayList<>();
+
+        String desc = "asdads asdkjlads alkjsdalskjd a lskjdalds kjad lkjads lkjadslkj asdlkja dslkjasdl kjasdlk jadslkja sdlkjas lkdjads lkjsadlk jadslk jadslkjasdlkj aslkdja slkd jasld jaldskj alkjds";
+
+        WishList one = new WishList("jule2022", desc, null);
+        WishList two = new WishList("jule2022", desc, null);
+        WishList three = new WishList("jule2022", desc, null);
+        WishList four = new WishList("jule2022", desc, null);
+        WishList five = new WishList("jule2022", desc, null);
+        WishList six = new WishList("jule2022", desc, null);
+        WishList seven = new WishList("jule2022", desc, null);
+        //WishList eight = new WishList("jule2022", desc, null);
+
+        list.add(one);
+        list.add(two);
+        list.add(three);
+        list.add(four);
+        list.add(five);
+        list.add(six);
+        list.add(seven);
+        //list.add(eight);
+
+
+        return list;
+    }
+
+
 }
--- src/main/java/com/example/wishlistwebapplication/model/WishList.java
+++ src/main/java/com/example/wishlistwebapplication/model/WishList.java
@@ -3,5 +3,39 @@ package com.example.wishlistwebapplication.model;
 import java.util.List;
 
 public class WishList {
+
+  private String name;
+  private String description;
   private List<Wish> wishList;
+
+
+  public WishList(String name, String description, List<Wish> wishList){
+    this.name = name;
+    this.description = description;
+    this.wishList = wishList;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public List<Wish> getWishList() {
+    return wishList;
+  }
+
+  public void setWishList(List<Wish> wishList) {
+    this.wishList = wishList;
+  }
 }
